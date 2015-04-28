934e0ed501b806fb5c4089494aa33b5cf830cc7c
ericguyader <ericguyader@yahoo.fr>
2015-04-28 23:11:14 +0200

    ajout dico

---
+++ ajax/dico-b.js
@@ -0,0 +1,16 @@
+﻿[
+    {
+        "mot": "Bateau",
+        "defs": [
+                    "Véhicule pouvant évoluer sur l'eau",
+                    "Passe-partout"
+                ]
+    },
+     {
+         "mot": "Bascule",
+         "defs": [
+                    "Mécanisme penchant d'un côté puis d'un autre.",
+                    "Balance"
+                 ]
+     }
+]
